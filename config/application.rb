3c35105b780cff8be861dfd449c3ade56d72bae0
Luther <lutheroto@gmail.com>
2017-04-13 17:45:29 -0400

    Delete Application#call.

--- config/application.rb
+++ config/application.rb
@@ -4,10 +4,5 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 
 module BlocBooks
   class Application < BlocWorks::Application
-    def call env
-      response = super(env)
-      response[2][0] = "This is BlocBooks!<br>" + response[2][0]
-      response
-    end
   end
 end
