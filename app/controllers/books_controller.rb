734c843ee850f84b57eb50bd92d8cd8eeab4bfae
Luther <lutheroto@gmail.com>
2017-04-22 06:24:06 -0400

    Stub out the CRUD actions.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,16 @@ class BooksController < BlocWorks::Controller
   def welcome
     render(:welcome, book: "Eloquent Ruby")
   end
+
+  def create
+    'create'
+  end
+
+  def update
+    'update'
+  end
+
+  def delete
+    'delete'
+  end
 end
